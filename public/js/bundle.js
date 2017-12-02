cba6ceff5f3b0c41e41e65f43996ad14cd16aa96
tarya85 <emanuela.grimaldi85@gmail.com>
2017-12-02 16:47:56 +0100

    added parallax in home and added all icons

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -182,9 +182,16 @@ var scrollme = function (a) {
 }(jQuery);
 'use strict';
 
-/*$(window).load(function() {
-    $(".site-loader").fadeOut("slow");;
-});*/
+$(window).load(function () {
+    //$(".site-loader").fadeOut("slow");;
+
+    if ($(window).width() > 1170 && (msieversion() >= 10 || msieversion() == 'other')) {
+        checkParallax();
+    } else {
+        console.log('no parallax');
+        $('.full-animated-columns .full-column-text').css("transform", "none");
+    }
+});
 
 jQuery(document).ready(function ($) {
 
@@ -200,11 +207,21 @@ jQuery(document).ready(function ($) {
     if ($('.site-home-slider').length > 0) {
         $('.site-home-slider').css('minHeight', $(window).height());
 
-        $(window).on({ 'orientationchange resize': function orientationchangeResize() {
+        $(window).on({
+            'orientationchange resize': function orientationchangeResize() {
                 $('.site-home-slider').css('minHeight', $(window).height());
             }
         });
     }
+
+    $(window).on("resize orientationchange", function (event) {
+        if ($(window).width() > 1170 && (msieversion() >= 10 || msieversion() == 'other')) {
+            checkParallax();
+        } else {
+            console.log('no parallax');
+            $('.full-animated-columns .full-column-text').css("transform", "none");
+        }
+    });
 
     $(window).scroll(function () {
         if ($(window).scrollTop() >= $('.site-header').outerHeight(true)) {
@@ -251,7 +268,87 @@ jQuery(document).ready(function ($) {
     });
 });
 
+function checkParallax() {
+    if ($('.full-animated-columns').length > 0 && $('.full-animated-columns .full-column-text').length > 0 && $('.full-animated-columns .full-column-image').length > 0) {
+        var parallaxElements = $('.full-animated-columns .full-column-text'),
+            parallaxQuantity = parallaxElements.length;
+        var containerHeight = parallaxElements.parent().outerHeight(true);
+
+        var valueToScroll = 1;
+        var percentage = 0.2;
+
+        window.requestAnimationFrame(function () {
+
+            for (var y = 0; y < parallaxQuantity; y++) {
+                var currentElement = parallaxElements.eq(y);
+                var scrolled = $(window).scrollTop();
+                var eTop = currentElement.offset().top;
+
+                if ($(window).width() > 1600) {
+                    valueToScroll = 1.8;
+                } else if ($(window).width() > 1400 && $(window).width() <= 1600) {
+                    valueToScroll = 1.5;
+                } else if ($(window).width() > 1300 && $(window).width() <= 1400) {
+                    valueToScroll = 1.1;
+                }
+
+                currentElement.css({
+                    'transform': 'translate3d(0,' + (eTop - scrolled * valueToScroll) * -percentage + 'px, 0)'
+                });
+            }
+        });
+
+        $(window).on('scroll', function () {
+
+            var valueToScroll = 1;
+            var percentage = 0.2;
+
+            window.requestAnimationFrame(function () {
+
+                for (var y = 0; y < parallaxQuantity; y++) {
+                    var currentElement = parallaxElements.eq(y);
+                    var scrolled = $(window).scrollTop();
+                    var eTop = currentElement.offset().top;
+
+                    if ($(window).width() > 1600) {
+                        valueToScroll = 1.8;
+                    } else if ($(window).width() > 1400 && $(window).width() <= 1600) {
+                        valueToScroll = 1.5;
+                    } else if ($(window).width() > 1300 && $(window).width() <= 1400) {
+                        valueToScroll = 1.1;
+                    }
+
+                    currentElement.css({
+                        'transform': 'translate3d(0,' + (eTop - scrolled * valueToScroll) * -percentage + 'px, 0)'
+                    });
+                }
+            });
+        });
+    }
+
+    return true;
+}
+
 function is_touch_device() {
     return 'ontouchstart' in window // works on most browsers
     || navigator.maxTouchPoints; // works on IE10/11 and Surface
+};
+
+function msieversion() {
+
+    var toReturn = '';
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        toReturn = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    } else if (trident > 0) {
+        var rv = ua.indexOf('rv:');
+        toReturn = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    } else {
+        toReturn = 'other';
+    }
+
+    return toReturn;
 };
--- source/js/custom.js
+++ source/js/custom.js
@@ -1,8 +1,15 @@
-/*$(window).load(function() {
-    $(".site-loader").fadeOut("slow");;
-});*/
+$(window).load(function () {
+    //$(".site-loader").fadeOut("slow");;
 
-jQuery(document).ready(function($){
+    if ($(window).width() > 1170 && (msieversion() >= 10 || msieversion() == 'other')) {
+        checkParallax();
+    } else {
+        console.log('no parallax');
+        $('.full-animated-columns .full-column-text').css("transform", "none");
+    }
+});
+
+jQuery(document).ready(function ($) {
 
     /*$('#searchform').submit(function(e) { 
         var s = $( this ).find("#s").val($.trim($( this ).find("#s").val()));
@@ -11,24 +18,33 @@ jQuery(document).ready(function($){
             $('#s').focus();
         }
     });*/
-    
-    //center content in home header and set it 100% height
-    if($('.site-home-slider').length > 0){
-        $('.site-home-slider').css('minHeight',$(window).height());
 
-        $(window).on({'orientationchange resize' : function(){
-               $('.site-home-slider').css('minHeight',$(window).height());
+    //center content in home header and set it 100% height
+    if ($('.site-home-slider').length > 0) {
+        $('.site-home-slider').css('minHeight', $(window).height());
+
+        $(window).on({
+            'orientationchange resize': function () {
+                $('.site-home-slider').css('minHeight', $(window).height());
             }
         });
     }
 
-    $(window).scroll(function(){
+    $(window).on("resize orientationchange", function (event) {
+        if ($(window).width() > 1170 && (msieversion() >= 10 || msieversion() == 'other')) {
+            checkParallax();
+        } else {
+            console.log('no parallax');
+            $('.full-animated-columns .full-column-text').css("transform", "none");
+        }
+    });
+
+    $(window).scroll(function () {
         if ($(window).scrollTop() >= ($('.site-header').outerHeight(true))) {
             $('.site-header').addClass('fixed-header');
             $('.site-header .site-top').fadeOut(0);
 
-        }
-        else {
+        } else {
             $('.site-header').removeClass('fixed-header');
             $('.site-header .site-top').fadeIn(500);
 
@@ -36,43 +52,132 @@ jQuery(document).ready(function($){
 
         if ($(this).scrollTop() > 100) {
             $('.scrollToTop').fadeIn();
-          } else {
+        } else {
             $('.scrollToTop').fadeOut();
-          }
-    });
-
-    if ($('img.aligncenter').length > 0) {
-      $('img.aligncenter').parent('p').css('textAlign','center');
-    }
-
-    $("a[href^='#']").not('[href="#"]').on("click tap", function(e) {
-        e.preventDefault();
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 500);
-            return false;
-          }
         }
     });
 
-    $('.site-header .site-header__hamburger').on("click tap", function(e) {
+    if ($('img.aligncenter').length > 0) {
+        $('img.aligncenter').parent('p').css('textAlign', 'center');
+    }
+
+    $("a[href^='#']").not('[href="#"]').on("click tap", function (e) {
         e.preventDefault();
-        $('.site-overlay').css('width','100%');
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
     });
 
-    $('.site-overlay .close').on("click tap", function(e) {
+    $('.site-header .site-header__hamburger').on("click tap", function (e) {
         e.preventDefault();
-        $('.site-overlay').css('width','0%');
+        $('.site-overlay').css('width', '100%');
     });
 
-})
+    $('.site-overlay .close').on("click tap", function (e) {
+        e.preventDefault();
+        $('.site-overlay').css('width', '0%');
+    });
+
+});
+
+function checkParallax() {
+    if ($('.full-animated-columns').length > 0 && $('.full-animated-columns .full-column-text').length > 0 && $('.full-animated-columns .full-column-image').length > 0) {
+        var parallaxElements = $('.full-animated-columns .full-column-text'),
+            parallaxQuantity = parallaxElements.length;
+        var containerHeight = parallaxElements.parent().outerHeight(true);
+
+        var valueToScroll = 1;
+        var percentage = 0.2;
+
+        window.requestAnimationFrame(function () {
+
+            for (var y = 0; y < parallaxQuantity; y++) {
+                var currentElement = parallaxElements.eq(y);
+                var scrolled = $(window).scrollTop();
+                var eTop = currentElement.offset().top;
+
+                if ($(window).width() > 1600) {
+                    valueToScroll = 1.8;
+                } else if ($(window).width() > 1400 && $(window).width() <= 1600) {
+                    valueToScroll = 1.5;
+                } else if ($(window).width() > 1300 && $(window).width() <= 1400) {
+                    valueToScroll = 1.1;
+                } 
+
+                currentElement.css({
+                    'transform': 'translate3d(0,' + (((eTop - scrolled * valueToScroll) * -percentage)) + 'px, 0)'
+                });
+
+            }
+        });
+
+
+
+
+        $(window).on('scroll', function () {
+
+            var valueToScroll = 1;
+            var percentage = 0.2;
+
+            window.requestAnimationFrame(function () {
+
+                for (var y = 0; y < parallaxQuantity; y++) {
+                    var currentElement = parallaxElements.eq(y);
+                    var scrolled = $(window).scrollTop();
+                    var eTop = currentElement.offset().top;
+
+                    if ($(window).width() > 1600) {
+                        valueToScroll = 1.8;
+                    } else if ($(window).width() > 1400 && $(window).width() <= 1600) {
+                        valueToScroll = 1.5;
+                    } else if ($(window).width() > 1300 && $(window).width() <= 1400) {
+                        valueToScroll = 1.1;
+                    }
+
+
+
+                    currentElement.css({
+                        'transform': 'translate3d(0,' + ((eTop - scrolled * valueToScroll) * -percentage) + 'px, 0)'
+                    });
+
+                }
+            });
+
+        });
+    }
+
+    return true;
+}
 
 
 function is_touch_device() {
-  return 'ontouchstart' in window        // works on most browsers
-      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+    return 'ontouchstart' in window // works on most browsers
+        ||
+        navigator.maxTouchPoints; // works on IE10/11 and Surface
+};
+
+function msieversion() {
+
+    var toReturn = '';
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        toReturn = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    } else if (trident > 0) {
+        var rv = ua.indexOf('rv:');
+        toReturn = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    } else {
+        toReturn = 'other';
+    }
+
+    return toReturn;
 };
